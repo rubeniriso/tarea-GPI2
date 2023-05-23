a8637812e56d9b694759702d8db0e021bca9f067
Ruben Iriso <rubenirisoe@gmail.com>
2023-05-23 18:47:51 +0200

    [] - Commit inicial con el proyecto y el .sh de automatizacion

---
+++ automatization.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Clonar el repositorio
+git clone https://github.com/rubeniriso/tarea-GPI2.git
+cd Mifare\ Classic\ Tool/
+
+# Compilar
+./gradlew
+
+# Ejecutar el analisis de codigo estático con PMD
+# Lo he añadido al PATH y el resultado está en .gitignore
+pmd check ./Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ./results.txt
