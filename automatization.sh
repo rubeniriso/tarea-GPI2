fa2a6f96634ebffb71f27ebb4a0680cf7a401877
Ruben Iriso <rubenirisoe@gmail.com>
2023-05-23 18:54:15 +0200

    [] - Modificacion de automatization.sh

--- automatization.sh
+++ automatization.sh
@@ -2,11 +2,11 @@
 
 # Clonar el repositorio
 git clone https://github.com/rubeniriso/tarea-GPI2.git
-cd Mifare\ Classic\ Tool/
+cd tarea-GPI2/Mifare\ Classic\ Tool/
 
 # Compilar
 ./gradlew
 
 # Ejecutar el analisis de codigo estático con PMD
 # Lo he añadido al PATH y el resultado está en .gitignore
-pmd check ./Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ./results.txt
+pmd check ../Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ./results.txt
