81053cdb68af2adb7c33688df0282483b7f6ae9a
Ruben Iriso <rubenirisoe@gmail.com>
2023-05-23 19:34:16 +0200

    [] - Cambio a automatization.sh, creo que funcional

--- automatization.sh
+++ automatization.sh
@@ -5,10 +5,9 @@ git clone https://github.com/rubeniriso/tarea-GPI2.git
 cd tarea-GPI2/Mifare\ Classic\ Tool/
 
 # Compilar
-export ANDROID_HOME=/home/alumno/android/
-export ANDROID_SDK_ROOT=/home/alumno/android/
+export ANDROID_HOME=/home/alumno/Android/
+export ANDROID_SDK_ROOT=/home/alumno/Android/Sdk
 ./gradlew :app:build
 
 # Ejecutar el analisis de codigo estático con PMD
-# Lo he añadido al PATH y el resultado está en .gitignore
 /home/alumno/Descargas/pmd-bin-7.0.0-rc2/bin/pmd check -d ../Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ./results.txt
