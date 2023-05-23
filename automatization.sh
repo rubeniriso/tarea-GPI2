efe1768ac215b6f405d9a3b87277b78bc153b9e6
Ruben Iriso <rubenirisoe@gmail.com>
2023-05-23 19:48:34 +0200

    [] - automatization.sh ahora mueve los entregables a su carpeta correspondiente, README.md actualizado

--- automatization.sh
+++ automatization.sh
@@ -6,8 +6,15 @@ cd tarea-GPI2/Mifare\ Classic\ Tool/
 
 # Compilar
 export ANDROID_HOME=/home/alumno/Android/
-export ANDROID_SDK_ROOT=/home/alumno/Android/Sdk
+export ANDROID_SDK_ROOT=/home/alumno/Android/
 ./gradlew :app:build
 
+
+
 # Ejecutar el analisis de codigo estático con PMD
-/home/alumno/Descargas/pmd-bin-7.0.0-rc2/bin/pmd check -d ../Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ./results.txt
+/home/alumno/Descargas/pmd-bin-7.0.0-rc2/bin/pmd check -d ../Mifare\ Classic\ Tool/ --rulesets=rulesets/java/quickstart.xml -r ../Entregables/results.txt
+
+
+# Mover los ejecutables a la carpeta Ejecutables
+mv app/build/outputs/apk/release/app-release-unsigned.apk ../Entregables/
+mv app/build/outputs/apk/debug/app-debug.apk ../Entregables/
